b32b5263f0e69613b755de9697f764cc8c4614e1
mkanunnikov <git4shift>
2017-01-27 16:53:35 +0600

    Fix formatting

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ config.setString("-dict", "vocabular.dict");
 config.setString("-lm", modeldir + "en-us.lm.bin");
 var decoder = new ps.Decoder(config);
 
-fs.readFile("../../pocketsphinx/test/data/goforward.raw", function(err, data) {
+fs.readFile("../../pocketsphinx/test/data/goforward.raw", function (err, data) {
     if (err) throw err;
     decoder.startUtt();
     decoder.processRaw(data, false, false);
