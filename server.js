50bc9236be8c424ea30198ae0eb3ccdbfc8bdc3d
mkanunnikov <git4shift>
2017-01-27 17:39:36 +0600

    Merge from

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const config = require('./config');
+// const config = require('./config');
 var ps = require('pocketsphinx').ps;
 
 modeldir = "../../pocketsphinx/model/en-us/"
